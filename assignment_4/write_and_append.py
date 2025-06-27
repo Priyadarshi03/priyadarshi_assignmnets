a2621b4f5c312965da3977a6ecb7657f73865b43
Priyadarshi03 <priyadarshianand03@gmail.com>
2025-06-27 17:13:47 +0530

    added task 2

---
+++ assignment_4/write_and_append.py
@@ -0,0 +1,14 @@
+'''
+Task 2: Write and Append Data to a File
+ 
+Problem Statement: Write a Python program that:
+1.   Takes user input and writes it to a file named output.txt.
+2.   Appends additional data to the same file.
+3.   Reads and displays the final content of the file.
+'''
+
+def write_and_append():
+
+    input_text = input("Enter the text to write in the ouput file: ")
+
+    with open("output.txt",'w') as :
