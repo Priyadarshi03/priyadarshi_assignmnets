f7385b3f971bf21ba5daecc2a17d4ff8cee62174
Priyadarshi03 <priyadarshianand03@gmail.com>
2025-06-27 16:27:11 +0530

    added task 1

---
+++ assignment_4/read_file_handle_error.py
@@ -0,0 +1,21 @@
+'''
+Task 1: Read a File and Handle Errors 
+Problem Statement:  Write a Python program that:
+1.   Opens and reads a text file named sample.txt.
+2.   Prints its content line by line.
+3.   Handles errors gracefully if the file does not exist.
+
+'''
+def read_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            print("File content:")
+            for line in file:
+                print(line.strip())
+    except FileNotFoundError:
+        print(f"Error: The file {file_name} was not found.")
+    except Exception as e:
+        print(f"An unexpected error occured: {e}") 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+file_name = "sample.txt"
+read_file(file_name)
