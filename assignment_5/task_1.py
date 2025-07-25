9ace54e12b6d0673ceac8b5f5d829a3e95b66aa5
Priyadarshi03 <priyadarshianand03@gmail.com>
2025-07-25 18:56:52 +0530

    assign 5 task1

---
+++ assignment_5/task_1.py
@@ -0,0 +1,15 @@
+''' 
+Task 1: Create a Dictionary of Student Marks
+Problem Statement: Write a Python program that:
+1.   Creates a dictionary where student names are keys and their marks are values.
+2.   Asks the user to input a student's name.
+3.   Retrieves and displays the corresponding marks.
+4.   If the student’s name is not found, display an appropriate message.
+'''
+
+student_data = {'maya':20, 'aman':30, 'karan':40, 'rahul':20}
+
+student_name = input("Enter the student name: ")
+
+student_marks = 
+
